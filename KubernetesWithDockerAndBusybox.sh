06f612549ee485a8e987c024724181453ca19d02
laughingbiscuit <sean@laughingbiscuit.com>
2023-01-19 19:17:17 +0000

    readability

--- KubernetesWithDockerAndBusybox.sh
+++ KubernetesWithDockerAndBusybox.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 set -xe
 
-echo "Lets check that docker is installed, this is our only dependency to run this demo!" > /dev/null
+echo \
+  "Lets check that docker is installed, this is our only dependency to run this demo!" > /dev/null
+read PressEnterToContinue
 docker version
-read EnterToContinue
+read PressEnterToContinue
 
-echo "Lets install k3d, a minimal kubernetes distro by rancher" > /dev/null
+echo \
+  "Lets install k3d, a minimal kubernetes distro by rancher" > /dev/null
+read PressEnterToContinue
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-read EnterToContinue
+read PressEnterToContinue
 
-echo "And create our cluster" > /dev/null
+echo \
+  "And create our cluster" > /dev/null
+read PressEnterToContinue
 k3d cluster create mycluster
+read PressEnterToContinue
 
-echo "And check it is up" > /dev/null
+echo \
+  "And check it is up" > /dev/null
+read PressEnterToContinue
 kubectl get nodes
-read EnterToContinue
+read PressEnterToContinue
 
