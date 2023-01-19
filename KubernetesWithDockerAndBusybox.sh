406e8390583ffd9fd9c9d378ae133d64f6ad1ad2
laughingbiscuit <sean@laughingbiscuit.com>
2023-01-19 19:11:05 +0000

    create k3d cluster

--- KubernetesWithDockerAndBusybox.sh
+++ KubernetesWithDockerAndBusybox.sh
@@ -4,5 +4,15 @@ set -xe
 echo "Lets check that docker is installed, this is our only dependency to run this demo!" > /dev/null
 docker version
 read EnterToContinue
+
+echo "Lets install k3d, a minimal kubernetes distro by rancher" > /dev/null
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+read EnterToContinue
+
+echo "And create our cluster" > /dev/null
+k3d cluster create mycluster
+
+echo "And check it is up" > /dev/null
+kubectl get nodes
+read EnterToContinue
 
