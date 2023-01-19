011a1dcbf3ea5c2f0c8bfe0a4e0cf13a92b8ce76
laughingbiscuit <sean@laughingbiscuit.com>
2023-01-19 18:43:06 +0000

    install k3d

--- KubernetesWithDockerAndBusybox.sh
+++ KubernetesWithDockerAndBusybox.sh
@@ -4,4 +4,5 @@ set -xe
 echo "Lets check that docker is installed, this is our only dependency to run this demo!" > /dev/null
 docker version
 read EnterToContinue
-blahhh
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
