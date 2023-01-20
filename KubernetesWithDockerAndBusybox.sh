2ef035dcccae45bac5026044ca39c766bf6224d3
laughingbiscuit <sean@laughingbiscuit.com>
2023-01-20 21:19:45 +0000

    minimal cluster install

--- KubernetesWithDockerAndBusybox.sh
+++ KubernetesWithDockerAndBusybox.sh
@@ -16,7 +16,7 @@ read PressEnterToContinue
 echo \
   "And create our cluster" > /dev/null
 read PressEnterToContinue
-k3d cluster create mycluster
+k3d cluster create mycluster --no-deploy=traefik,servicelb,metrics-server@server:*
 sleep 2
 read PressEnterToContinue
 
