33e30620d9c97e94d8bfbf3326e6058319bdaf15
laughingbiscuit <sean@laughingbiscuit.com>
2023-01-21 15:35:34 +0000

    deploy nginx as a sample

--- KubernetesWithDockerAndBusybox.sh
+++ KubernetesWithDockerAndBusybox.sh
@@ -19,7 +19,7 @@ read PressEnterToContinue
 echo \
   "And create our cluster" > /dev/null
 read PressEnterToContinue
-k3d cluster create mycluster --k3s-arg "--disable=traefik,servicelb,metrics-server,local-storage,coredns@server:*"
+k3d cluster create mycluster -p "8081:80@loadbalancer"
 read PressEnterToContinue
 
 echo \
@@ -28,3 +28,49 @@ read PressEnterToContinue
 kubectl get nodes
 read PressEnterToContinue
 
+echo \
+  "Let's deploy nginx as a test" > /dev/null
+read PressEnterToContinue
+kubectl create deployment nginx --image=nginx
+kubectl wait --for=condition=available deployment.apps/nginx --timeout 30s
+read PressEnterToContinue
+
+echo \
+  "Let's create a service to expose it" > /dev/null
+read PressEnterToContinue
+kubectl create service clusterip nginx --tcp=80:80
+read PressEnterToContinue
+
+echo \
+  "Now create the ingress" > /dev/null
+read PressEnterToContinue
+
+cat << EOF | kubectl apply -f -
+
+# apiVersion: networking.k8s.io/v1beta1 # for k3s < v1.19
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: nginx
+  annotations:
+    ingress.kubernetes.io/ssl-redirect: "false"
+spec:
+  rules:
+  - http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service:
+            name: nginx
+            port:
+              number: 80
+EOF
+read PressEnterToContinue
+
+echo \
+  "Lets call it every 5 seconds until nginx is accessible" > /dev/null
+read PressEnterToContinue
+while ! curl -sf localhost:8081 > /dev/null; do sleep 5; done
+curl localhost:8081
+read PressEnterToContinue
