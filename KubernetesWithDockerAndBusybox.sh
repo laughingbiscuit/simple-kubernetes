d3f5b4c266a8c728b56b615c14a4cf65bb6d22d8
laughingbiscuit <sean@laughingbiscuit.com>
2023-01-21 15:39:51 +0000

    improve readability

--- KubernetesWithDockerAndBusybox.sh
+++ KubernetesWithDockerAndBusybox.sh
@@ -4,45 +4,45 @@ set -xe
 cat welcome.txt && sleep 1
 read PressEnterToContinue
 
-echo \
-  "Lets check that docker is installed, this is our only dependency to run this demo!" > /dev/null
+echo >/dev/null \
+  "Lets check that docker is installed, this is our only dependency to run this demo!"
 read PressEnterToContinue
 which docker
 read PressEnterToContinue
 
-echo \
-  "Lets install k3d, a minimal kubernetes distro by rancher" > /dev/null
+echo >/dev/null \
+  "Lets install k3d, a minimal kubernetes distro by rancher"
 read PressEnterToContinue
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 read PressEnterToContinue
 
-echo \
-  "And create our cluster" > /dev/null
+echo >/dev/null \
+  "And create our cluster"
 read PressEnterToContinue
 k3d cluster create mycluster -p "8081:80@loadbalancer"
 read PressEnterToContinue
 
-echo \
-  "And check it is up" > /dev/null
+echo >/dev/null \
+  "And check it is up"
 read PressEnterToContinue
 kubectl get nodes
 read PressEnterToContinue
 
-echo \
-  "Let's deploy nginx as a test" > /dev/null
+echo >/dev/null \
+  "Let's deploy nginx as a test"
 read PressEnterToContinue
 kubectl create deployment nginx --image=nginx
 kubectl wait --for=condition=available deployment.apps/nginx --timeout 30s
 read PressEnterToContinue
 
-echo \
-  "Let's create a service to expose it" > /dev/null
+echo >/dev/null \
+  "Let's create a service to expose it"
 read PressEnterToContinue
 kubectl create service clusterip nginx --tcp=80:80
 read PressEnterToContinue
 
-echo \
-  "Now create the ingress" > /dev/null
+echo >/dev/null \
+  "Now create the ingress"
 read PressEnterToContinue
 
 cat << EOF | kubectl apply -f -
@@ -68,9 +68,10 @@ spec:
 EOF
 read PressEnterToContinue
 
-echo \
-  "Lets call it every 5 seconds until nginx is accessible" > /dev/null
+echo >/dev/null \
+  "Lets call it every 5 seconds until nginx is accessible"
 read PressEnterToContinue
 while ! curl -sf localhost:8081 > /dev/null; do sleep 5; done
 curl localhost:8081
 read PressEnterToContinue
+
